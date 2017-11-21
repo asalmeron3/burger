a1a10e81c0fedfeca3189153a9523d8c5c74626f
Arturo Salmeron <asalmeron3@gatech.edu>
2017-11-21 10:07:37 -0500

    WIP: need to complete handlebars and orm

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,8 @@
--- In this file, write insert queries to populate the `burgers` table with at least three entries.
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
+INSERT INTO burgers (burger_name) VALUES ('Black & Bleu');
+INSERT INTO burgers (burger_name) VALUES ('Bean Burger');
+INSERT INTO burgers (burger_name) VALUES ('Vegetables Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Swiss Mushroom');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+
